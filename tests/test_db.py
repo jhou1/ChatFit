64230deadebe8e9004622061f57da505977bb276
Jianwei Hou <2202520+jhou1@users.noreply.github.com>
2026-06-29 16:08:34 +0800

    test: update the tests of new db schema and e2e scenarios

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,18 +1,47 @@
-from models import TrainingSessionInfo
-from utils.db import add_training_session, get_training_sessions_of_last_n_days, init_db
 from datetime import datetime, timedelta
+import sqlite3
+
+from models import TrainingSession, TrainingSet, RecordTrainingInput
+from utils.db import add_training_session, get_training_sessions_of_last_n_days, init_db
 
 def test_add_training_session(tmp_path):
     db_path = tmp_path / "training_session_test.db"
     init_db(db_path)
 
-    training_session = TrainingSessionInfo(
-        date=datetime.now().date().isoformat(),
-        practice_name="test",
-        note="feel good practice."
+    test_input = RecordTrainingInput(
+        date=datetime.now().date(),
+        sessions=[
+            TrainingSession(
+                practice_name="Squat",
+                practice_type="weighted",
+                note="Testing",
+                sets=[TrainingSet(set_number=1, weight=100, reps=10)]
+            )
+        ],
+        confirm_new_practices=True 
     )
-    row_id = add_training_session(training_session, db_path)
-    assert isinstance(row_id, int)
+
+    result = add_training_session(test_input, db_path)
+    assert result == "Training log saved successfully!"
+
+    with sqlite3.connect(db_path) as conn:
+        conn.row_factory = sqlite3.Row
+
+        cursor = conn.cursor()
+        cursor.execute("SELECT * from training_sessions")
+        rows = cursor.fetchall()
+        assert len(rows) == 1
+
+        cursor.execute(
+            """
+            SELECT p.name, p.type from training_sessions t, practices p
+            WHERE t.practice_id = p.id
+            AND t.note == "Testing"
+            """
+        )
+        practice = cursor.fetchall()[0]
+        assert practice["name"] == "Squat"
+        assert practice["type"].lower() == "weighted"
 
 
 def test_get_training_sessiosn_of_last_n_days(tmp_path):
@@ -21,12 +50,20 @@ def test_get_training_sessiosn_of_last_n_days(tmp_path):
 
     # prepare test data
     for i in range(1,8):
-        ts = TrainingSessionInfo(
-           date=(datetime.now().date() - timedelta(i)).isoformat(),
-           practice_name=f"test{i}",
-           note="feel good",
+        test_input = RecordTrainingInput(
+            date=datetime.now().date() - timedelta(i),
+            sessions=[
+                TrainingSession(
+                    practice_name=f"Test{i}",
+                    practice_type="bodyweight",
+                    note="Testing",
+                    sets=[TrainingSet(set_number=1, weight=100, reps=10)]
+                )
+            ],
+            confirm_new_practices=True 
         )
-        add_training_session(ts, db_path)
+
+        add_training_session(test_input, db_path)
 
     training_sessions = get_training_sessions_of_last_n_days(7, db_path)
     assert len(training_sessions) == 7
--- tests/test_training_recorder.py
+++ tests/test_training_recorder.py
@@ -4,7 +4,8 @@ from datetime import datetime, timedelta
 
 from agents.training_recorder import make_record_training_graph
 from utils.llm_factory import LLMConfig
-from utils.db import init_db
+from utils.db import init_db, add_training_session
+from models import RecordTrainingInput, TrainingSession, TrainingSet
 
 from langchain_core.messages import HumanMessage
 
@@ -25,76 +26,111 @@ def llm_config():
     )
 
 @pytest.mark.e2e
-def test_make_record_training_graph(llm_config, temp_db_path):
-    message = HumanMessage(content="I ran 5km in 30 minutes yesterday. RPE was around 5. Felt great!")
-    initial_state = {
-        "messages": [message]
-    }
+def test_e2e_save_training_session(llm_config, temp_db_path):
+    message = HumanMessage(content="I ran 5km in 30 minutes yesterday, rpe was around 5. Felt great!")
+    state = {"messages": [message]}
     app = make_record_training_graph(llm_config, temp_db_path)
-    app.invoke(initial_state)
+    response = app.invoke(state)
+
+    # because addition of new practices must be approved
+    # we create a new message to approve adding the record
+    agent_reply = response["messages"][-1].content[0]["text"]
+    assert "running" in agent_reply.lower()
+    assert "?" in agent_reply.lower()
+
+    state["messages"].extend([
+        response["messages"][-1],
+        HumanMessage(content="Yes, please add it as distance practice.")
+    ])
+    app.invoke(state)
 
     # agent should have inserted db records
     with sqlite3.connect(temp_db_path) as conn:
         conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
-        cursor.execute(
-            """
-            SELECT * from training_sessions
-            """
-        )
+        cursor.execute("SELECT * from training_sessions")
         rows = cursor.fetchall()
 
         assert len(rows) == 1
 
         # verify that data has been correctly inserted
         saved_session = rows[0]
-        assert saved_session["practice_name"].lower() == "running"
         assert saved_session["rpe"] == 5
-        assert saved_session["duration"] == 30
-        assert saved_session["date"] == (datetime.now() - timedelta(days=1)).date().isoformat()
+        assert saved_session["created_at"] == (datetime.now() - timedelta(days=1)).date().isoformat()
+
+        cursor.execute("SELECT * from training_sets")
+        rows = cursor.fetchall()
+        saved_sets = rows[0]
+        assert len(rows) == 1
+        assert saved_sets["distance"] == 5.0
+        assert saved_sets["duration"] == 30.0
 
 @pytest.mark.e2e
-def test_record_training_with_double_sessions(llm_config, temp_db_path):
-    initial_state = {
-        "messages": ["I ran 10km in 30 minutes yesterday, then I snatched a 24kg kettlebell 150 times in 15 minutes. RPE was 10. Felt tired"]
-    }
+def test_e2e_save_multiple_training_sessions(llm_config, temp_db_path):
+    message = HumanMessage(content="I ran 10km in 30 minutes yesterday, then I snatched a 24kg kettlebell, 10 sets and 10 reps per set. RPE was 10.")
+    state = {"messages": [message]}
     app = make_record_training_graph(llm_config, temp_db_path)
-    app.invoke(initial_state)
+    response = app.invoke(state)
+
+    agent_reply = response["messages"][-1].content[0]["text"]
+    assert "running" in agent_reply.lower()
+    assert "kettlebell" in agent_reply.lower()
+    assert "?" in agent_reply.lower()
+
+    state["messages"].extend([
+        response["messages"][-1],
+        HumanMessage(content="Yes, please add running as distance practice and add kettlebell snatch as weighted practice.")
+    ])
+    app.invoke(state)
+
 
     # agent should have inserted db records
     with sqlite3.connect(temp_db_path) as conn:
         conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
-        cursor.execute(
-            """
-            SELECT * from training_sessions
-            """
-        )
-        rows = cursor.fetchall()
 
+        cursor.execute("SELECT * FROM training_sessions")
+        rows = cursor.fetchall()
         assert len(rows) == 2
 
-        kb_session = next((row for row in rows if "kettlebell" in row["practice_name"].lower()), None)
-        assert kb_session is not None
-        assert kb_session["duration"] == 15
-        assert kb_session["rpe"] == 10
+        cursor.execute("SELECT * FROM training_sets WHERE weight == 24")
+        rows = cursor.fetchall()
+        assert len(rows) == 10
+
+        cursor.execute("SELECT * FROM training_sets WHERE distance == 10")
+        rows = cursor.fetchall()
+        assert len(rows) == 1
+
+        cursor.execute("SELECT name, type FROM practices")
+        rows = cursor.fetchall()
+        for row in rows:
+            assert row["name"].lower() in ["running", "kettlebell snatch"]
+            assert row["type"].lower() in ["weighted", "distance"]
 
 @pytest.mark.e2e
-def test_retrieve_training(llm_config, temp_db_path):
-    # data preparation
-    message = HumanMessage(content="I swung the 32KG kettlebell for 200 times today!")
+def test_retrieve_training_sessions(llm_config, temp_db_path):
+    # prepare test data
+    for i in range(1, 8):
+        test_input = RecordTrainingInput(
+            date=datetime.now().date() - timedelta(i),
+            sessions=[
+                TrainingSession(
+                    practice_name=f"Squat",
+                    practice_type="bodyweight",
+                    note="Testing",
+                    sets=[TrainingSet(set_number=1, reps=10*i)]
+                )
+            ],
+            confirm_new_practices=True 
+        )
+
+        add_training_session(test_input, temp_db_path)
+
+    message = HumanMessage(content="Tell me about in the past 7 days, what trainings and practice types I had practiced.")
     state = {"messages": [message]}
     app = make_record_training_graph(llm_config, temp_db_path)
-    app.invoke(state)
-
-    new_message = HumanMessage(content="What training did I do today?")
-    new_state = {"messages": [new_message]}
-    app = make_record_training_graph(llm_config, temp_db_path)
-    response = app.invoke({"messages": new_state["messages"]})
+    response = app.invoke({"messages": state["messages"]})
     final_text = response["messages"][-1].content[0]["text"]
 
-    assert "200" in final_text
-    assert "kettlebell" in final_text.lower()
-    assert "32kg" in final_text.lower()
-
-    
+    assert "squat" in final_text.lower()
+    assert "bodyweight" in final_text.lower()
